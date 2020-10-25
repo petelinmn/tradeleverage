2023ebd92b5da43edc027ee937f0138601fc7199
Maksim Petelin <mpetelin@accesssoftek.com>
2020-10-25 10:32:33 +0500

    first chart is added

---
+++ src/Chart/Chart1.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+interface IChart1Props {
+
+}
+
+interface IChart1State {
+  options:any,
+  series: any
+}
+
+class Chart1 extends Component<IChart1Props, IChart1State> {
+  constructor(props: IChart1Props) {
+    super(props);
+
+    this.state = {
+      options: {
+        chart: {
+          id: "basic-bar"
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+        }
+      },
+      series: [
+        {
+          name: "series-1",
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="row">
+          <div className="mixed-chart">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="500"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chart1;
